39d21cbff20611f84070ffd475f00b56e6c2f3af
cristian <cregueiromartinez@danielcastelao.org>
2024-11-10 14:40:10 +0100

    commit inicial del microservicio

---
+++ nonrelational-prd-query/src/main/java/com/cristianProyectoAD/nonrelational_prd_query/NonrelationalPrdQueryApplication.java
@@ -0,0 +1,13 @@
+package com.cristianProyectoAD.nonrelational_prd_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonrelationalPrdQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonrelationalPrdQueryApplication.class, args);
+	}
+
+}
---
+++ nonrelational-prd-query/src/test/java/com/cristianProyectoAD/nonrelational_prd_query/NonrelationalPrdQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianProyectoAD.nonrelational_prd_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonrelationalPrdQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
