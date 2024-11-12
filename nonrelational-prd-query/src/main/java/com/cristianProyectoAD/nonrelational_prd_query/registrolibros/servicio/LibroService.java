9cf62f48ead7265292f98842b8d40c8a8b00a416
Cristian <cregueiromartinez@danielcastelao.org>
2024-11-12 09:10:42 +0100

    documentacion servicio registro libros

--- nonrelational-prd-query/src/main/java/com/cristianProyectoAD/nonrelational_prd_query/registrolibros/servicio/LibroService.java
+++ nonrelational-prd-query/src/main/java/com/cristianProyectoAD/nonrelational_prd_query/registrolibros/servicio/LibroService.java
@@ -6,13 +6,24 @@ import com.cristianProyectoAD.nonrelational_prd_query.registrolibros.repositorio
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Clase servicio donde llamamos al repositorio de mongo para el registro de libros
+ * @author cristian
+ * @version 1.0
+ */
 @Service
 public class LibroService {
 
+    //inyyección de dependencias en el repositorio de mongo
     @Autowired
     private LibroRepositorioMongo librosRepositorio;
 
+    /**
+     * Metodo para guardar libros en el repositorio de mongo
+     * @param libro el libro a guardar
+     */
     public void saveBook(LibroRegistroDTO libro) {
+        //creamos un objeto de tipo libro y lo guardamos en el repositorio de mongo
         Libros librosGuardar = new Libros(libro.getIsbn(), libro.getAutor(),
                 libro.getNombre(), libro.getFechaLectura(), libro.getFechaRegistro());
         librosRepositorio.save(librosGuardar);
