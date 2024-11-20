91c597577e28190b1e681469b356537f59561ff6
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 20:06:42 +0100

    creando rest controller consulta libro por nombre

--- nonrelational-prd-query/src/main/java/com/cristianProyectoAD/nonrelational_prd_query/consultas/nombre/controlador/LibroNombreRestController.java
+++ nonrelational-prd-query/src/main/java/com/cristianProyectoAD/nonrelational_prd_query/consultas/nombre/controlador/LibroNombreRestController.java
@@ -1,4 +1,28 @@
 package com.cristianProyectoAD.nonrelational_prd_query.consultas.nombre.controlador;
 
+import com.cristianProyectoAD.nonrelational_prd_query.consultas.nombre.servicio.LibroNombreService;
+import com.cristianProyectoAD.nonrelational_prd_query.registrolibros.modelo.Libros;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/nonrelational-prd-query/libros/consultas/")
 public class LibroNombreRestController {
+
+    private final LibroNombreService libroNombreService;
+
+    public LibroNombreRestController(LibroNombreService libroNombreService) {
+        this.libroNombreService = libroNombreService;
+    }
+
+    @GetMapping("nombre{nombre}")
+    public ResponseEntity<List<Libros>> getLibrosByNombreMongo(@PathVariable String nombre) {
+        List<Libros> libros = libroNombreService.getLibrosByNombre(nombre);
+        return ResponseEntity.ok(libros);
+    }
 }
