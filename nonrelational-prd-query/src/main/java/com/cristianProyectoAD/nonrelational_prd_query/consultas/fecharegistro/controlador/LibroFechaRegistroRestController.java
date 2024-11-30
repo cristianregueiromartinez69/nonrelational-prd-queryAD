414fed60a1115eba470e2d4f53c1e12bc0248200
cristian <cregueiromartinez@danielcastelao.org>
2024-11-30 16:17:58 +0100

    servicio restcontroller fecha registro

--- nonrelational-prd-query/src/main/java/com/cristianProyectoAD/nonrelational_prd_query/consultas/fecharegistro/controlador/LibroFechaRegistroRestController.java
+++ nonrelational-prd-query/src/main/java/com/cristianProyectoAD/nonrelational_prd_query/consultas/fecharegistro/controlador/LibroFechaRegistroRestController.java
@@ -1,4 +1,29 @@
 package com.cristianProyectoAD.nonrelational_prd_query.consultas.fecharegistro.controlador;
 
+import com.cristianProyectoAD.nonrelational_prd_query.consultas.fecharegistro.servicio.LibroFechaRegistroService;
+import com.cristianProyectoAD.nonrelational_prd_query.registrolibros.modelo.Libros;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/nonrelational-prd-query/libros/consultas")
 public class LibroFechaRegistroRestController {
+
+    private final LibroFechaRegistroService libroFechaRegistroService;
+
+    public LibroFechaRegistroRestController(LibroFechaRegistroService libroFechaRegistroService) {
+        this.libroFechaRegistroService = libroFechaRegistroService;
+    }
+
+    @GetMapping("/fecharegistro")
+    public ResponseEntity<List<Libros>> getLibrosFechaRegistro(LocalDate fechaInicio, LocalDate fechaFin) {
+        List<Libros> librosList = libroFechaRegistroService.getLibrosFechaRegistro(fechaInicio, fechaFin);
+        return ResponseEntity.ok(librosList);
+    }
+
 }
